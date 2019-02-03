40f678a836cc22d1b07dcbfb8f4aac0a3704d4f7
Andrzej Godlewski <andgod@wp.pl>
2019-02-03 13:38:33 +0100

    Pierwszy commit

---
+++ src/Main.java
@@ -0,0 +1,96 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] tablica = new String[6][6];
+
+        printTree1(tablica);
+        System.out.println();
+        printTree2(tablica);
+        System.out.println();
+        printTree3(tablica);
+        System.out.println();
+        printTree4(tablica);
+        System.out.println();
+        printTree5(tablica);
+        System.out.println();
+        
+
+
+    }
+
+    private static void printTree1(String[][] tablica) {
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println();
+
+            for (int j = 0; j < tablica.length; j++) {
+                tablica[i][j] = "*";
+                if (j <= i)
+                    System.out.print(tablica[i][j]);
+            }
+        }
+    }
+
+    private static void printTree2(String[][] tablica) {
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println();
+
+            for (int j = 0; j < tablica.length; j++) {
+                if (i == j)
+                    tablica[i][j] = "*";
+                else
+                    tablica[i][j] = " ";
+                System.out.print(tablica[i][j]);
+
+            }
+        }
+    }
+
+    private static void printTree3(String[][] tablica) {
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println();
+
+            for (int j = 0; j < tablica.length; j++) {
+                if (i == 0)
+                    tablica[i][j] = "*";
+                else if (j == 0)
+                    tablica[i][j] = "*";
+                else if (i == 5)
+                    tablica[i][j] = "*";
+                else if (j == 5)
+                    tablica[i][j] = "*";
+                else
+                    tablica[i][j] = " ";
+                System.out.print(tablica[i][j]);
+            }
+        }
+    }
+
+    private static void printTree4(String[][] tablica) {
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println();
+            for (int j = 0; j < tablica.length; j++) {
+                if (i == j)
+                    tablica[i][j] = "*";
+                else if ((i + j) == tablica.length - 1)
+                    tablica[i][j] = "*";
+                else
+                    tablica[i][j] = " ";
+                System.out.print(tablica[i][j]);
+            }
+        }
+    }
+
+    private static void printTree5(String[][] tablica) {
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println();
+            for (int j = 0; j < tablica.length; j++) {
+                if (i <= j)
+                    tablica[i][j] = "*";
+                else
+                    tablica[i][j] = " ";
+                System.out.print(tablica[i][j]);
+            }
+        }
+    }
+}
